71359e8fb604e334b56d5495f73932eefaa3d126
N3crates <gtoz6164@gmail.com>
2026-05-09 10:51:25 -0600

    Se agregaron las rutas de navegacion

---
+++ Frontend/src/services/authService.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const API_URL = 'http://localhost:3000/api/auth'
+
+export const loginRequest = async(credentials) => {
+    const response = await axios.post(
+        `${API_URL}/login`,
+        credentials 
+    )
+    return response.data
+}
---
+++ Frontend/src/views/audit/AuditView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-10">
+    <h1 class="text-4xl font-bold">
+      Audit
+    </h1>
+  </div>
+</template>
